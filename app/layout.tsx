c44a9c4a29c14dc732d68b6a9b24301feeacd86b
Fabricio Martins <fabriciocsmartins@gmail.com>
2024-06-04 20:28:25 -0300

    [feat] implement landing page and logo

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import '@styles/globals.css'
+import { ReactNode } from 'react';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover and share AI prompts"
+}
+
+interface RootLayoutProps {
+    children: ReactNode;
+}
+
+const RootLayout = ({ children} : RootLayoutProps) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className="main">
+                <div className="gradient"/>
+            </div>
+
+            <main className='app'>
+                <Nav />
+                {children}
+            </main>
+        </body> 
+    </html>
+  )
+}
+
+export default RootLayout 

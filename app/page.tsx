6cb25731fedf146fcda764ec48c5873e14ef2ee8
Fabricio Martins <fabriciocsmartins@gmail.com>
2024-06-17 18:26:02 -0300

    [feat] create prompt creation page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
-import Feed from "@components/Feed"
+import { useState } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import Form from "@components/Form"
 
-const Home = () => {
+const CreatePrompt = () => {
+    const [ submitting, setSubmitting ] = useState(false)
+    const [ post, setPost ] = useState({
+        prompt: '',
+        tag: ''
+    })
+
+
+
     return (
-        <section className="w-full flex-center flex-col">
-            <h1 className="head_text text-center">
-                Discover & Share
-                <br  className="max-md:hidden"/>
-                <span className="orange_gradient text-center"> AI-Powered Prompts</span>
-            </h1>
-            <p className="desc text-center">
-                Promtopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
-            </p>
-
-            <Feed />
-        </section>
+        <Form 
+            type="Create"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        />
     )
 }
 
-export default Home
+export default CreatePrompt
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,8 +1,41 @@
 import React from 'react'
 
-const Form = () => {
+
+interface RootLayoutProps {
+  type: string
+  post: {prompt: string, tag:string}
+  setPost: any
+  submitting: boolean
+  handleSubmit: string
+}
+
+const Form = ({ type, post, setPost, submitting, handleSubmit }: RootLayoutProps) => {
   return (
-    <div>Form</div>
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className='head_text, text-left'>
+        <span className='blue_gradient'> {type} Post </span>
+      </h1>
+
+      <p className='desc text-left max-w-md'>
+        {type} and share amazing prompts with the world, let your imagination run wild with any AI-powered platform.
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className='mt-10 w-full max-w-2x1 flex flex-col gap7 glassmorphism'
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">Your AI Prompt</span>
+        </label>
+        <textarea 
+          value={post.prompt}
+          onChange={(e) => setPost({ ...post, prompt: e.target.value})} 
+          placeholder='Write your prompt here'
+          className="form_textarea"
+          >
+        </textarea>
+      </form>
+    </section>
   )
 }
 

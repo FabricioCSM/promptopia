3995edd357fac386559a7abb14127be7d35649e7
Fabricio Martins <fabriciocsmartins@gmail.com>
2024-06-25 20:46:02 -0300

    [feat] implement creation of prompt backend, front and model, db

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-
+import React from 'react';
+import Link from 'next/link';
 
 interface RootLayoutProps {
   type: string
   post: {prompt: string, tag:string}
   setPost: any
   submitting: boolean
-  handleSubmit: string
+  handleSubmit: any
 }
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }: RootLayoutProps) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
-      <h1 className='head_text, text-left'>
+      <h1 className='head_text text-left'>
         <span className='blue_gradient'> {type} Post </span>
       </h1>
 
@@ -26,14 +26,36 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }: RootLayoutProps
       >
         <label>
           <span className="font-satoshi font-semibold text-base text-gray-700">Your AI Prompt</span>
+        
+          <textarea 
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value})} 
+            placeholder='Write your prompt here'
+            required
+            className="form_textarea"
+            />
         </label>
-        <textarea 
-          value={post.prompt}
-          onChange={(e) => setPost({ ...post, prompt: e.target.value})} 
-          placeholder='Write your prompt here'
-          className="form_textarea"
-          >
-        </textarea>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">Tag</span>
+        
+          <input 
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value})} 
+            placeholder='#tag'
+            required
+            className="form_input"
+            />
+        </label>
+
+        <div className='flex-end mx-3 mb-5 gap-4'>
+          <Link href="/" className='text-gray-500'>
+            Cancel
+          </Link>
+
+          <button type='submit' disabled={submitting} className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white '>
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
       </form>
     </section>
   )
